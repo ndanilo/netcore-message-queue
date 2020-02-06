5ddcbbf70b2cbcf8024292497b561970ae659356
madedanilonovais <danilo.novais@madeinweb.com.br>
2020-02-06 20:24:06 -0300

    basic structure to message queue

---
+++ MessageQueueSetup/BasicStructure/Receiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project - Receiver");
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "sample", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Received: {0}", message);
+                };
+
+                while (true)
+                    channel.BasicConsume(queue: "sample", autoAck: true, consumer: consumer);
+            }
+
+            Console.WriteLine("Finished!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MessageQueueSetup/BasicStructure/Sender/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "sample", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+
+                while (true)
+                {
+                    Console.Write("message: ");
+                    var message = Console.ReadLine();
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", "sample", basicProperties: null, body: body);
+                    Console.WriteLine("sent");
+                }
+            }
+
+            Console.WriteLine("Finished!");
+            Console.ReadKey();
+        }
+    }
+}
